9d8359fcb7fb8873af7c9f01ff6acb35a7d9597b
Weliton Silva <welitonjosedasilva@gmail.com>
2023-06-27 10:31:24 -0300

    subindo tela

---
+++ aula2.py
@@ -0,0 +1,8 @@
+entrada = input('Você deseja entrar ou sair ? ')
+if entrada == 'entrar':
+    (print('Você entrou'))
+elif entrada == 'sair':
+    print('Você saiu')
+else:
+    print('Você não digitou entrar nem sair.')
+    

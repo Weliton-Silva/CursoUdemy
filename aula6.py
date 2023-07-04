6c6a8ab5c785de0487557f9504cb2f16f3d76c39
Weliton Silva <welitonjosedasilva@gmail.com>
2023-07-04 09:45:26 -0300

    subindo tela

--- aula6.py
+++ aula6.py
@@ -1,4 +1,4 @@
-horas = int(input('Que horas são ? '))
+horas = int(input('Digite a hora em números inteiros: '))
 if horas >= 0 and horas <= 11:
     print('Bom dia !')
 elif horas >= 12 and horas <= 17:
--- aula7.py
+++ aula7.py
@@ -1,5 +1,7 @@
-nome = input('Digite seu nome. ')
-if len(nome) <= 4:
+nome = input('Digite seu nome: ')
+if len(nome) <= 1:
+    print('Digite mais de uma letra.')
+elif len(nome) <= 4:
     print('Seu nome é curto !')
 elif len(nome) <= 6:
     print('Seu nome é normal !')
